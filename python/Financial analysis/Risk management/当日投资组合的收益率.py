9244dafc748156993d64b1da1f8f5dc6401cfad1
keyzhi <ken361@163.com>
2023-01-03 22:39:32 +0800

    js this 及py 金融分析

---
+++ python/Financial analysis/Risk management/当日投资组合的收益率.py
@@ -0,0 +1,23 @@
+# 普通方法计算一日投资组合收益率
+
+#2020.5.25 4只股票的日涨跌幅
+return_May25 = [-0.035099,-0.013892,0.005848,0.021242]
+
+#4只股票的配置权重
+weight_list = [0.15,0.20,0.25,0.40]
+
+#股票数量
+n = len(weight_list)
+
+
+#创建存放每只股票收益率与配置权重数乘积的空列表
+return_weight = []
+
+for i in range(n):
+    return_weight.append(return_May25[i] * weight_list[i]) #将计算结果存放在列表末尾
+
+
+return_port_May25 = sum(return_weight)
+
+print('2020年5月25日投资组合的收益率',round(return_port_May25,6))  #round（x，n）方法将返回x的值，该值四舍五入到小数点后的n位数字
+
---
+++ python/基础篇/69.匿名函数.py
@@ -0,0 +1,14 @@
+'''
+    匿名函数:
+     lambda表达式
+     语法:
+        变量 = lambda 参数,参数2,参数3...:返回值
+
+'''
+
+fn = lambda a,b:a+b
+
+ret = fn(12,13)
+
+print(ret)
+
