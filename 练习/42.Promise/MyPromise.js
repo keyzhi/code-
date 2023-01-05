3a43c33b296fa325d5bbb9855b2ecf8e14c1802b
keyzhi <ken361@163.com>
2023-01-06 00:12:31 +0800

    promise代码

---
+++ 练习/42.Promise/MyPromise.js
@@ -0,0 +1,44 @@
+const PENDING = 'PENDING',
+      FULFILLED = 'FULFILLED',
+      REJECTED = 'REJECTED';
+
+class MyPromise{
+    constructor(executor) {
+        this.status = PENDING;
+        this.value = undefined;
+        this.reason = undefined;
+        
+        const resolve = (value)=>{
+            if(this.status === PENDING){
+                this.status = FULFILLED
+                this.value = value
+            }
+        }
+        const reject = (reason)=>{
+            if(this.status === PENDING){
+                this.status = REJECTED
+                this.reason = reason
+            }
+        }
+        try {
+            executor(resolve,reject)
+        } catch (error) {
+            reject(error)
+        }
+        
+    
+    }
+
+    then(onFulfilled,onRejected) {
+        if(this.status === FULFILLED){
+            onFulfilled(this.value)
+        }
+
+        if(this.status === REJECTED) {
+            onRejected(this.reason)
+        }
+    }
+}
+
+
+module.exports = MyPromise
--- 练习/42.Promise/index.js
+++ 练习/42.Promise/index.js
@@ -1,3 +1,20 @@
-let promise = new Promise(()=>{ //excutor 执行器
+// let promise = new Promise((resolve,reject)=>{ //executor 执行器
 
+const MyPromise = require('./MyPromise')
+
+// })
+
+
+let myPromise = new MyPromise((resolve,reject)=>{
+    // resolve('success')
+    // reject('reject')
+    throw new Error('Exception:Error')
 })
+
+myPromise.then((val)=>{
+    console.log('success1111:',val)
+},(err)=>{
+    console.log('reject1111:',err)
+})
+
+// console.log('myPromise:',MyPromise.prototype)
